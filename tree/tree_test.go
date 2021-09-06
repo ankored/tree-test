09e33a02de9d9a197e72dcb5874ba02e73a8fc8d
James Holdren <jamesdholdren@gmail.com>
2021-09-06 17:37:31 -0400

    Add tests for solidifying tree API

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,6 +1,7 @@
 package tree_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/ankored/tree-test/tree"
@@ -28,6 +29,74 @@ func TestAddCreatesNode(t *testing.T) {
 				Value: "something",
 			},
 		},
+	}, tr.Root); diff != "" {
+		t.Errorf("Tree mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestAddRequiresParentToBeDir(t *testing.T) {
+	tr := tree.New(&tree.Node{
+		Type: tree.NodeTypeDir,
+		Childs: map[string]*tree.Node{
+			"child": &tree.Node{
+				Type: tree.NodeTypeVal,
+			},
+		},
+	})
+
+	err := tr.AddChild("/child/grandchild", &tree.Node{
+		Type:  tree.NodeTypeVal,
+		Value: "something",
+	})
+	var dirErr tree.ErrNotDir
+	if !errors.As(err, &dirErr) {
+		t.Fatalf("expected ErrNotDir, instead got: %s", err)
+	}
+}
+
+func TestAddRequiresAllParentsToExist(t *testing.T) {
+	tr := tree.New(&tree.Node{
+		Type: tree.NodeTypeDir,
+		Childs: map[string]*tree.Node{
+			"child": &tree.Node{
+				Type: tree.NodeTypeDir,
+			},
+		},
+	})
+
+	err := tr.AddChild("/child/not-here/child", &tree.Node{
+		Type:  tree.NodeTypeVal,
+		Value: "something",
+	})
+
+	var errNF tree.ErrNotFound
+	if !errors.As(err, &errNF) {
+		t.Fatalf("expected ErrNotFound, instead got: %s", err)
+	}
+
+	if errNF.Path != "child/not-here" {
+		t.Fatalf("expected error path to be %s, instead got: %s", "child/not-here", errNF.Path)
+	}
+}
+
+func TestAddNilRemovesNode(t *testing.T) {
+	tr := tree.New(&tree.Node{
+		Type: tree.NodeTypeDir,
+		Childs: map[string]*tree.Node{
+			"child": &tree.Node{
+				Type: tree.NodeTypeVal,
+			},
+		},
+	})
+
+	err := tr.AddChild("/child", nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	if diff := cmp.Diff(&tree.Node{
+		Type:   tree.NodeTypeDir,
+		Childs: map[string]*tree.Node{},
 	}, tr.Root); diff != "" {
 		t.Errorf("Tree mismatch (-want +got):\n%s", diff)
 	}
